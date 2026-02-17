f4854fe0ec6d5cd4298507a8fa0021b17bf8c83e
bedell <2310501007@mahasiswa.upnvj.ac.id>
2026-02-18 02:35:42 +0700

    Update basePath in next.config.ts for production and fix animation easing in GraduationInvitation componentj

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/greeting' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/Invinitation' : '',
   images: {
     unoptimized: true,
   },
